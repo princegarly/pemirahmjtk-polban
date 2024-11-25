12d851cfeed0eb5e15c0807978fb1469b9aa83b0
thisisgarly <thisisgarly@gmail.com>
2024-11-25 15:10:43 +0700

    FIX : View - Dashboard

---
+++ resources/views/components/master/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div class="footer-left">Copyright &copy; Pemira HMJTK POLBAN - 2024</div>
+    <div class="footer-right">Template by <a href="https://nauval.in/">Muhamad Nauval Azhar</a> & Develop by <a href="https://arcakranusantara.com/">Arcakranusantara</a></div>
+</footer>
